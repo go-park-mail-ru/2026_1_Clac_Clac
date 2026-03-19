67e29e2caffd4f117463a19aee34cdf9a88700e9
Nisakoo <modeniki10@gmail.com>
2026-03-19 23:34:24 +0300

    feat: add more fields in access log

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -1,12 +1,23 @@
 package middleware
 
 import (
+	"errors"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog"
+)
+
+const (
+	xRealIPHeader       = "X-Real-IP"
+	xForwardedForHeader = "X-Forwarded-For"
+)
+
+var (
+	ErrInteralServerError = errors.New("something went wrong")
 )
 
 type LoggerResponseWriter struct {
@@ -19,12 +30,25 @@ func (w *LoggerResponseWriter) WriteHeader(code int) {
 	w.ResponseWriter.WriteHeader(code)
 }
 
+func GetRealIP(r *http.Request) string {
+	if ip := r.Header.Get(xRealIPHeader); ip != "" {
+		return ip
+	}
+
+	if ip := r.Header.Get(xForwardedForHeader); ip != "" {
+		return strings.Split(ip, ",")[0]
+	}
+
+	return r.RemoteAddr
+}
+
 func LoggerMiddleware(logger *zerolog.Logger) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			requestStart := time.Now()
 
 			requestLogger := logger.With().Str("request-id", uuid.New().String()).Logger()
+
 			ctx := requestLogger.WithContext(r.Context())
 
 			responseWriter := &LoggerResponseWriter{
@@ -33,11 +57,22 @@ func LoggerMiddleware(logger *zerolog.Logger) mux.MiddlewareFunc {
 			}
 			next.ServeHTTP(responseWriter, r.WithContext(ctx))
 
+			requestDuration := time.Since(requestStart)
+
+			// TODO: решить вопрос с request.Body
 			requestLogger.Info().
 				Str("method", r.Method).
-				Str("path", r.URL.Path).
+				Str("remote_addr", r.RemoteAddr).
+				Str("url", r.URL.Path).
+				Dur("work_time", requestDuration).
 				Int("status", responseWriter.statusCode).
-				Dur("latency", time.Since(requestStart)).
+				Str("user_agent", r.UserAgent()).
+				Str("host", r.Host).
+				Str("real_ip", GetRealIP(r)).
+				Int64("content_length", r.ContentLength).
+				Str("start_time", requestStart.Format(time.RFC3339)).
+				Str("duration_human", requestDuration.String()).
+				Int64("duration_ms", requestDuration.Milliseconds()).
 				Msg("request processed")
 		})
 	}
--- internal/middleware/logger_test.go
+++ internal/middleware/logger_test.go
@@ -11,17 +11,19 @@ import (
 )
 
 func TestLoggerMiddleware(t *testing.T) {
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	t.Run("request without error", func(t *testing.T) {
+		res := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
 
-	logger := zerolog.New(nil)
+		logger := zerolog.New(nil)
 
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.NotEqual(t, zerolog.DefaultContextLogger, zerolog.Ctx(r.Context()), "logger should not be default")
+		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.NotEqual(t, zerolog.DefaultContextLogger, zerolog.Ctx(r.Context()), "logger should not be default")
+		})
+		m := middleware.LoggerMiddleware(&logger).Middleware(h)
+
+		m.ServeHTTP(res, req)
 	})
-	m := middleware.LoggerMiddleware(&logger).Middleware(h)
-
-	m.ServeHTTP(res, req)
 }
 
 func TestLoggerResponseWriter(t *testing.T) {
@@ -45,5 +47,41 @@ func TestLoggerResponseWriter(t *testing.T) {
 		loggerResponseWriter.WriteHeader(http.StatusInternalServerError)
 
 		assert.Equal(t, http.StatusInternalServerError, res.Code, "http codes must be equal")
+	})
+}
+
+func TestGetRealIP(t *testing.T) {
+	const (
+		testIpAddr          = "8.8.8.8"
+		multipleIpAddr      = "8.8.8.8,10.0.0.1"
+		xRealIpHeader       = "X-Real-IP"
+		xForwardedForHeader = "X-Forwarded-For"
+	)
+
+	t.Run("X-Real-IP header", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		req.Header.Set(xRealIpHeader, testIpAddr)
+
+		actualIp := middleware.GetRealIP(req)
+
+		assert.Equal(t, testIpAddr, actualIp, "IPs must be equal")
+	})
+
+	t.Run("X-Forwarded-For header, single ip", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		req.Header.Set(xForwardedForHeader, testIpAddr)
+
+		actualIp := middleware.GetRealIP(req)
+
+		assert.Equal(t, testIpAddr, actualIp, "IPs must be equal")
+	})
+
+	t.Run("X-Forwarded-For header, multiple ips", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		req.Header.Set(xForwardedForHeader, multipleIpAddr)
+
+		actualIp := middleware.GetRealIP(req)
+
+		assert.Equal(t, testIpAddr, actualIp, "IPs must be equal")
 	})
 }
