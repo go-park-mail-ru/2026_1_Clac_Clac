40402f7e76f3a3a7a9786efddbc6cbdc74944d65
Mofyart <busygin.artem05mail.ru>
2026-04-25 12:39:21 +0300

    feat:add srvice create

--- internal/appeal/service/service.go
+++ internal/appeal/service/service.go
@@ -1,8 +1,18 @@
 package service
 
+import (
+	"context"
+
+	"github.com/go-park-mail-ru/2026_1_Clac_Clac/internal/appeal/service/dto"
+)
+
 type AppealRepository interface {
 }
 
 type Service struct {
 	rep AppealRepository
+}
+
+func (s *Service) CreateAppeal(ctx context.Context, appeal dto.EntityAppeal) error {
+
 }
