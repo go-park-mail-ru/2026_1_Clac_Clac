0b009faf6be669506a17e9c1ff70127443c5ee39
Mofyart <busygin.artem05mail.ru>
2026-05-06 04:41:11 +0300

    fix: metrics

--- pkg/metrics/http/metrics.go
+++ pkg/metrics/http/metrics.go
@@ -9,16 +9,17 @@ var (
 	HttpRequestTotal = promauto.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "http_requests_total",
-			Help: "Total number of HTTP requests by status",
+			Help: "Total number of HTTP requests by status and method",
 		},
-		[]string{"status"},
+		[]string{"status", "method"},
 	)
 
-	HttpRequestDuration = promauto.NewHistogram(
+	HttpRequestDuration = promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "http_request_duration_seconds",
 			Help:    "Duration of HTTP requests in seconds",
-			Buckets: []float64{0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
+			Buckets: []float64{0.005, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
 		},
+		[]string{"method"},
 	)
 )
