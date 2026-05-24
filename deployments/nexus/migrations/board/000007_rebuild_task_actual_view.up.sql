da991175fcdb67cd8bfbee3535b26ec1c4718237
Nisakoo <modeniki10@gmail.com>
2026-05-25 02:01:46 +0300

    fix: reorder fields in task_actual

--- deployments/nexus/migrations/board/000007_rebuild_task_actual_view.up.sql
+++ deployments/nexus/migrations/board/000007_rebuild_task_actual_view.up.sql
@@ -1,14 +1,9 @@
--- Включаем points в task_actual view
--- points был добавлен в таблицу task миграцией 000006,
--- но view task_actual его не содержала
-
 CREATE OR REPLACE VIEW task_actual AS
 SELECT
     t.task_id,
     t.task_link,
     t.author_link,
     t.created_at,
-    t.points,
     v.section_link,
     v.executer_link,
     v.title,
@@ -16,7 +11,8 @@ SELECT
     v.position,
     v.due_date,
     v.start,
-    v.status
+    v.status,
+    t.points
 FROM task t
 JOIN task_version v
   ON v.task_link = t.task_link
