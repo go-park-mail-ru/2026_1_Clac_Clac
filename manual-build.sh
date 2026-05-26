fb9289df411ce9407b284ad26784b66fdf4e4da2
Nisakoo <modeniki10@gmail.com>
2026-05-26 15:33:02 +0300

    feat: add manual deploy sh

---
+++ manual-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Использовать в ТОЛЬКО крайних случаях!!!
+
+echo -e "[DANGER] Использовать на свой страх и риск"
+echo -e "[DANGER] Предварительно надо войти в docker аккаунт"
+
+SERVICES=("appeal" "authorization" "board" "facade" "mail_sender" "rate_limiter" "user" "realtime")
+
+for $svc in "${SERVICES[@]}"; do
+    docker build --platform linux/amd64 \
+        -f "deployments/${svc}/Dockerfile" \
+        -t "nisakoo/nexus-${svc}:latest" \
+        .
+done
